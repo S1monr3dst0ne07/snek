49a30bcb82ff39f29a1252ce13e1c4937f52595f
S1monr3dst0ne07 <jaforsi.kozak@gmail.com>
2025-05-26 08:54:00 +0200

    implemented len and iter dunders for streamer

--- src/compiler/river.py
+++ src/compiler/river.py
@@ -7,6 +7,7 @@ from typing import Any
 class streamer:
     iterator : list[Any] = field(default_factory = lambda: [])
 
+
     def add(self, token):
         self.iterator.append(token)
     
@@ -15,6 +16,12 @@ class streamer:
 
     def pop(self):
         return self.iterator.pop(0)
+
+    def __len__(self):
+        return len(self.iterator)
+
+    def __iter__(self):
+        return iter(self.iterator)
 
     def __str__(self):
         return "\n".join(map(str, self.iterator))
